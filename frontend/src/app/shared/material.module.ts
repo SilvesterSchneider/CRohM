bca232413eef77d9c682ef69f8ebab57dda9a221
Michelle Martin <MartinMi81419@th-nuernberg.de>
2020-02-22 16:57:07 +0100

    Update material

--- frontend/src/app/shared/material.module.ts
+++ frontend/src/app/shared/material.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatSidenavModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatListModule,
-  MatTableModule,
-  MatButtonModule,
-  MatFormFieldModule,
-  MatAutocompleteModule,
-  MatInputModule,
-  MatCardModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
