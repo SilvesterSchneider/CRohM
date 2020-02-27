0ab94e87d44266f049ff97b9aad9837efe199921
Silvester <silvester_schneider@gmx.de>
2020-02-27 22:28:18 +0100

    fixing bug from microsoft

--- backend/ServiceLayer/SignInService.cs
+++ backend/ServiceLayer/SignInService.cs
@@ -9,13 +9,9 @@ namespace ServiceLayer
 {
     public class SignInService : SignInManager<User>
     {
-        public SignInService(
-            UserManager<User> userManager,
-            IHttpContextAccessor contextAccessor,
-            IUserClaimsPrincipalFactory<User> claimsFactory,
-            IOptions<IdentityOptions> optionsAccessor,
-            ILogger<SignInManager<User>> logger,
-            IAuthenticationSchemeProvider schemes) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes)
+        // had to include dll because fail from microsoft
+        // https://www.gitmemory.com/issue/aspnet/AspNetCore/12536/515210764
+        public SignInService(UserManager<User> userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<User> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<User>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<User> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
         {
         }
     }
