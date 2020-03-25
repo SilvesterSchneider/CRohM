cdc4bbae0ef6b9edbcfe0d337a157f88700d0b10
Makki93 <markus.dietl@mailbox.org>
2020-03-25 21:56:49 +0100

    contacts table created

---
+++ backend/ModelLayer/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using ModelLayer.Models.Base;
+
+namespace ModelLayer.Models
+{
+    public class Contact : BaseEntity
+    {
+        public string PreName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+    }
+}
