d7d369a2348960f6eda8d5ba173b647d1534e9fd
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-03-28 19:46:12 +0100

    added the controller and dto classes

---
+++ backend/ModelLayer/DataTransferObjects/OrganizationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModelLayer.DataTransferObjects
+{
+    public class OrganizationDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public AddressDto Address { get; set; }
+
+        public List<UserDto> Employees { get; set; }
+    }
+
+    public class OrganizationCreateDto
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+    }
+}
