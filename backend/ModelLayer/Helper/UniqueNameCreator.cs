b60f0a1bc1b22d0f4600dd7a8b708e3f2ba311d8
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-03-28 11:14:31 +0100

    added new class

---
+++ backend/ModelLayer/Helper/UniqueNameCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.Helper
+{
+    public class UniqueNameCreator
+    {
+        /// <summary>
+        /// Creates the unique username on base of pararmeters.
+        /// </summary>
+        /// <param name="firstName">the first name</param>
+        /// <param name="lastName">the last name</param>
+        /// <param name="numberOfEntryInTable">the actual amount of numbers in the table of users</param>
+        /// <returns>the unique string containing the username</returns>
+        public static string CreateUniqueName(string firstName, string lastName, int numberOfEntryInTable)
+        {
+            return lastName + firstName.Substring(0, 2) + numberOfEntryInTable;
+        }
+    }
+}
