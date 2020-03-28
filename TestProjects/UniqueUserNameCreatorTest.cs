9498c21a0ea2c78250024248ad8e6342f91183bb
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-03-28 11:23:14 +0100

    added test

---
+++ TestProjects/UniqueUserNameCreatorTest.cs
@@ -0,0 +1,22 @@
+﻿using ModelLayer.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestProjects
+{
+    public class UniqueUserNameCreatorTest
+    {
+        private const string FIRSTNAME = "razvan";
+        private const string LASTNAME = "matis";
+        private const int NUMBER = 81072;
+        private const string EXPECTED_USERNAME = "matisra81072";
+
+        [Fact]
+        public void TestIfUniqueUserNameIsBeingCreatedCorrectly()
+        {
+            Assert.Equal(EXPECTED_USERNAME, UniqueNameCreator.CreateUniqueName(FIRSTNAME, LASTNAME, NUMBER));
+        }
+    }
+}
