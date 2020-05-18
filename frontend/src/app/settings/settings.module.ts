7db49428fbfb54206bc71385a796830aa5a6b65f
JH <JH@DESKTOP-1OIAJSR>
2020-05-18 12:33:30 +0200

    Erweiterung so dass Nutzer geaendert auch werden koennen

--- frontend/src/app/settings/settings.module.ts
+++ frontend/src/app/settings/settings.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserComponent, UserDialogAddComponent } from './user/user.component';
+import { UserComponent, UserDialogEditComponent, UserDialogAddComponent } from './user/user.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { MaterialModule } from '../shared/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,7 +8,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [UserComponent, UserDialogAddComponent],
+  declarations: [UserComponent, UserDialogEditComponent, UserDialogAddComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule,
--- frontend/src/app/settings/user/user.component.ts
+++ frontend/src/app/settings/user/user.component.ts
@@ -15,13 +15,6 @@ interface Permission {
 
 
 
-// export interface DialogData {
-//   username: string;
-//   mail: string;
-//   firstname: string;
-//   lastname: string;
-// }
-
 
 @Component({
   selector: 'app-user',
@@ -34,18 +27,11 @@ export class UserComponent implements OnInit {
   dataSource = new BehaviorSubject<UserDto[]>([]);
   displayedColumns: string[] = ['username', 'mail', 'firstname', 'lastname', 'options'];
 
-  // userForm = this.fb.group({
-  //   email: ['', [Validators.email, Validators.required]],
-  //   firstName: ['', Validators.required],
-  //   lastName: ['', Validators.required]
-  // });
-
-
   constructor(// private readonly fb: FormBuilder,
     private readonly usersService: UsersService,
     private readonly authService: AuthService,
-    public addDialog: MatDialog) { }
-    
+    public dialog: MatDialog) { }
+
   public ngOnInit(): void {
     this.GetData();
   }
@@ -53,26 +39,30 @@ export class UserComponent implements OnInit {
   /**
    * Oeffnen des Dialogs zum Hinzufuegen eines neuen Nutzers
    */
-  openDialog(): void {
-    const dialogRef = this.addDialog.open(UserDialogAddComponent, {
-      width: '500px',
+  openAddDialog(): void {
+    const dialogRef = this.dialog.open(UserDialogAddComponent, {
+      width: '400px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      // Ausgabe auf der Konsole zum Debuggen
-      // console.log('The dialog was closed');
       // Update der angezeigten User-Liste
       this.GetData();
     });
   }
 
-  // private GetData() {
-  //   this.usersService.get().subscribe(users => {
-  //     this.dataSource.next(users);
-  //   });
-  // }
+  /**
+   * Oeffnen des Dialogs zum Bearbeiten eines Nutzers
+   */
+  openEditDialog(): void {
+    const dialogRef = this.dialog.open(UserDialogEditComponent, {
+      width: '400px',
+    });
 
-
+    dialogRef.afterClosed().subscribe(result => {
+      // Update der angezeigten User-Liste
+      this.GetData();
+    });
+  }
 
   private GetData() {
     this.usersService.get().subscribe(x => {
@@ -85,24 +75,15 @@ export class UserComponent implements OnInit {
     // TODO: call backend delete function
   }
 
-
   public OnPasswordReset(userId: number) {
     this.authService.changePassword(userId).subscribe(result => {
       console.log(result);
     });
   }
 
-  // public addUser() {
-  //   this.usersService.post(this.userForm.value).subscribe(user => {
-  //     console.log(user);
-  //     this.GetData();
-  //   });
-  // }
-
   public SetLockoutState(userId: number) {
     this.usersService.updateLockoutState(userId).subscribe(x => this.GetData());
   }
-
 }
 
 
@@ -116,7 +97,6 @@ export class UserComponent implements OnInit {
  * Komponente fuer den Modal-Dialog zum Hinzufuegen eines Nutzers
  */
 @Component({
-  // selector: 'user.component_dialog_add',
   templateUrl: 'user.component_dialog_add.html',
 })
 export class UserDialogAddComponent {
@@ -201,6 +181,103 @@ export class UserDialogAddComponent {
       }
     });
   }
+}
 
-  
+
+
+
+
+
+
+
+
+
+/**
+ * Komponente fuer den Modal-Dialog zum Hinzufuegen eines Nutzers
+ */
+@Component({
+  templateUrl: 'user.component_dialog_edit.html',
+})
+export class UserDialogEditComponent {
+
+  userForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required]
+  });
+
+  // Gruppenberechtigungen
+  // TODO: Liste aus Backend laden
+  groupPermissionList: Permission[] = [
+    { value: 'group_adm', viewValue: 'Administrator' },
+    { value: 'group_dat', viewValue: 'Datenschutzbeauftragter' },
+    { value: 'group_hig', viewValue: 'Hoch' },
+    { value: 'group_med', viewValue: 'Normal' },
+    { value: 'group_low', viewValue: 'Eingeschränkt' }
+  ];
+  groupPermissionDefault = ['group_low'];
+  groupPermission = new FormControl(this.groupPermissionDefault);
+
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly usersService: UsersService,
+    public dialogRef: MatDialogRef<UserDialogEditComponent>,
+    public dialog: MatDialog,
+    // @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
+
+
+  /**
+   * Funktion zum Schliessen des Dialogs
+   */
+  abortDialog(): void {
+    // Schliessen des Dialogs
+    this.dialogRef.close();
+  }
+
+
+  /**
+   * Funktion zum Speichen des neuen Users und Schliessen des Dialogs
+   */
+  public editUser() {
+    this.usersService.post(this.userForm.value).subscribe(user => {
+      // Ausgabe der Daten auf der Konsole zum Debuggen
+      // console.log(user);
+    });
+
+    // TODO: Speichern/Uebermitteln der Gruppenbrerchtigungen
+    // Ausgabe der Daten auf der Konsole zum Debuggen
+    console.log(this.groupPermission.value);
+
+    // Schliessen des Dialogs
+    this.dialogRef.close();
+  }
+
+
+  /**
+   * Funktion zum Aufruf des Confirm-Dialogs
+   */
+  confirmDialog(): void {
+    // Angezeigte Ueberschrift bzw. Nachricht im Confirm-Dialog
+    const message = `Wollen Sie den Vorgang wirklich abbrechen?`;
+    const dialogData = new ConfirmDialogModel('Warnung', message);
+
+    // Oeffnet den Confirm-Dialog mit definierter Ueberschrift bzw. Nachricht
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '400px',
+      data: dialogData
+    });
+
+    // Schliesst den Confirm-Dialog
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      // Wenn Nein angeklickt wurde passiert nichts.
+      // Wenn 'Ja' angeklickt wurde, dann wird die abortDialog-Funktion
+      // des drunterliegenden Dialogs (hier also Dialog zum Hinzufuegen
+      // eines Nutzers) geschlossen.
+      if (dialogResult === true) {
+        this.abortDialog();
+      }
+    });
+  }
 }
