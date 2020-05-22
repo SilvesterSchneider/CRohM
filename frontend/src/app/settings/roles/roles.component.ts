f69ce8830b1a09361ad62944d5b944b5d56ddb73
Silvester <silvester_schneider@gmx.de>
2020-05-22 11:22:05 +0200

    add genereic delete dialog

--- frontend/src/app/settings/roles/roles.component.ts
+++ frontend/src/app/settings/roles/roles.component.ts
@@ -3,6 +3,7 @@ import {ROLES, PERMISSIONS, IRoleTemp, IPermissionTemp} from './mock-roles';
 import {MatDialog} from '@angular/material/dialog';
 import { CreateRoleDialogComponent } from './create-role/create-role.component';
 import { UpdateRoleDialogComponent } from './update-role/update-role.component';
+import { DeleteEntryDialogComponent } from 'src/app/shared/delete-entry-dialog/delete-entry-dialog.component';
 
 interface LooseTableObject {
   [key: string]: any;
@@ -23,6 +24,7 @@ export class RolesComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   public ngOnInit(): void {
+    // TODO:
     // load data
     this.createDynamicColums();
     this.createTableData();
@@ -36,8 +38,9 @@ export class RolesComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-      // call backend to add role
-      // call backend for all roles
+        // TODO:
+        // call backend to add role
+        // call backend for all roles
         console.log(result);
       }
     });
@@ -54,14 +57,25 @@ export class RolesComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         if (result.delete) {
+          const deleteDialogRef = this.dialog.open(DeleteEntryDialogComponent, {
+            data: columnName
+          });
 
-        console.log(result);
+          deleteDialogRef.afterClosed().subscribe(deleteResult => {
+            if (deleteResult.delete) {
+              // TODO:
+              // call backend to delete role
+            }
+          });
+        } else {
+          // TODO:
+          // call backend to update role
+          // call backend for all roles
         }
-      // call backend to update role
-      // call backend for all roles
-        console.log(result);
       }
     });
+
+
   }
 
   private createTableData() {
--- frontend/src/app/settings/settings.module.ts
+++ frontend/src/app/settings/settings.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+
 import { UserComponent } from './user/user.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { MaterialModule } from '../shared/material.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { OverviewComponent } from './overview/overview.component';
-import {MatTabsModule} from '@angular/material/tabs';
 import { RolesComponent } from './roles/roles.component';
 import { CreateRoleDialogComponent } from './roles/create-role/create-role.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { UpdateRoleDialogComponent } from './roles/update-role/update-role.component';
+import { DeleteEntryDialogModule } from '../shared/delete-entry-dialog/delete-entry-dialog.module';
 
 
 
 @NgModule({
   declarations: [UserComponent, OverviewComponent, RolesComponent, CreateRoleDialogComponent, UpdateRoleDialogComponent],
   imports: [
+    DeleteEntryDialogModule,
     CommonModule,
     MatTabsModule,
     SettingsRoutingModule,
