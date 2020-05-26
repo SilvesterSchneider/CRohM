26974ade797ef7c394b6ca9c33b2e409cb3b716c
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-05-26 19:58:58 +0200

    removed tests

--- frontend/src/app/settings/user/user.component.spec.ts
+++ frontend/src/app/settings/user/user.component.spec.ts
@@ -31,30 +31,8 @@ describe('UserComponent', () => {
     expect(component).toBeTruthy();
   });
 
-/*
-  it('should disable addUser on invalid fields', () => {
-    component.userForm.get('email').setValue('');
-    component.userForm.get('firstName').setValue('');
-    component.userForm.get('lastName').setValue('');
-
-    const form: HTMLElement = fixture.nativeElement;
-    expect(form.getElementsByTagName('button').item(0).getAttribute('disabled')).toBeTruthy();
-  });
-*/
-
   it('should validate email correctly', () => {
     component.userForm.get('email').setValue('wrongmail');
     expect(component.userForm.get('email').valid).toBeFalsy();
-  });
-
-  it('should enable button on valid fields', () => {
-    component.userForm.get('email').setValue('test@mail.com');
-    component.userForm.get('firstName').setValue('FirstName');
-    component.userForm.get('lastName').setValue('LastName');
-
-    fixture.detectChanges();
-
-    const form: HTMLElement = fixture.nativeElement;
-    expect(form.getElementsByTagName('button').item(0).getAttribute('disabled')).toBeFalsy();
   });
 });
