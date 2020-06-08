1e28aa3423cb7eb80bacab0e4394b26018db17b4
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-06-08 20:05:45 +0200

    added events as dummy data

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService, OrganizationService, ContactDto, OrganizationDto } from '../shared/api-generated/api-generated';
 
+export class EventDto {
+  id: number;
+    date: string;
+    time: string;
+    name: string;
+    duration: number;
+    contacts: ContactDto[];
+}
+
+function getDummyEvents(): EventDto[] {
+  let events: EventDto[] = new Array<EventDto>();
+  events.push({
+    date: Date.now().toString(),
+    id: 1,
+    duration: 2,
+    name: 'testEvent1',
+    time: Date.now().toString(),
+    contacts: []
+  });
+  events.push({
+    date: Date.now().toString(),
+    id: 2,
+    duration: 4.5,
+    name: 'testEvent2',
+    time: Date.now().toString(),
+    contacts: []
+  });
+  return events;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,6 +39,7 @@ import { ContactService, OrganizationService, ContactDto, OrganizationDto } from
 export class HomeComponent implements OnInit {
   public contacts: ContactDto[] = new Array<ContactDto>();
   public organizations: OrganizationDto[] = new Array<OrganizationDto>();
+  public events: EventDto[] = new Array<EventDto>();
 
   constructor(private contactsService: ContactService,
     private organizationService: OrganizationService) { }
@@ -34,5 +65,6 @@ export class HomeComponent implements OnInit {
           this.organizations.push(x[index - 1]);
         }
       });
+    this.events = getDummyEvents();
   }
 }
