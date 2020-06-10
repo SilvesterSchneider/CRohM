9f35ef1e98452075c4ddb1fec4096bb70452024f
mi@geovisiongmbh.onmicrosoft.com <mi@geovisiongmbh.onmicrosoft.com>
2020-06-11 00:16:20 +0200

    Save

--- backend/ModelLayer/Models/Role.cs
+++ backend/ModelLayer/Models/Role.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using ModelLayer.Helper;
 
 namespace ModelLayer.Models
 {
     public class Role : IdentityRole<long>
     {
+        UserRight userRight;
     }
 }
