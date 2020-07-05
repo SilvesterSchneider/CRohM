7994e31d1b820604c7e2d1892585489cad550b93
Markus Dietl <markus.dietl@mailbox.org>
2020-07-05 21:44:13 +0200

    Added pdfSharp Api

---
+++ backend/RepositoryLayer/PdfGenerator.cs
@@ -0,0 +1,39 @@
+﻿using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using PdfSharp.Drawing;
+
+namespace RepositoryLayer
+{
+    class PdfGenerator
+    {
+        // Create a new PDF document
+        private PdfDocument doc = new PdfDocument();
+
+        doc.Info.Title = "Created with PDFsharp";
+ 
+        // Create an empty page
+        PdfPage page = document.AddPage();
+
+        // Get an XGraphics object for drawing
+        XGraphics gfx = XGraphics.FromPdfPage(page);
+
+        // Create a font
+        XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+        // Draw the text
+        gfx.DrawString("Hello, World!", font, XBrushes.Black,
+        new XRect(0, 0, page.Width, page.Height),
+        XStringFormats.Center);
+ 
+        // Save the document...
+        const string filename = "HelloWorld.pdf";
+        document.Save(filename);
+        // ...and start a viewer.
+        Process.Start(filename);
+    }
+}
+
+
