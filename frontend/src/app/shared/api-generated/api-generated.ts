4af7909852b833e58574042c114f8c350cdf2ce7
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-07-21 21:48:41 +0200

    added auto gen file

--- frontend/src/app/shared/api-generated/api-generated.ts
+++ frontend/src/app/shared/api-generated/api-generated.ts
@@ -1557,6 +1557,73 @@ export class ModificationEntryService {
         }
         return _observableOf<ModificationEntryDto[]>(<any>null);
     }
+
+    /**
+     * @param idQuery (optional) 
+     * @param modelDataType (optional) 
+     * @return successfully found
+     */
+    getSortedListByTypeAndId(idPath: string, idQuery?: number | undefined, modelDataType?: MODEL_TYPE | undefined): Observable<ModificationEntryDto[]> {
+        let url_ = this.baseUrl + "/api/home/{id}?";
+        if (idPath === undefined || idPath === null)
+            throw new Error("The parameter 'idPath' must be defined.");
+        url_ = url_.replace("{id}", encodeURIComponent("" + idPath));
+        if (idQuery === null)
+            throw new Error("The parameter 'idQuery' cannot be null.");
+        else if (idQuery !== undefined)
+            url_ += "id=" + encodeURIComponent("" + idQuery) + "&";
+        if (modelDataType === null)
+            throw new Error("The parameter 'modelDataType' cannot be null.");
+        else if (modelDataType !== undefined)
+            url_ += "modelDataType=" + encodeURIComponent("" + modelDataType) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processGetSortedListByTypeAndId(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processGetSortedListByTypeAndId(<any>response_);
+                } catch (e) {
+                    return <Observable<ModificationEntryDto[]>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<ModificationEntryDto[]>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processGetSortedListByTypeAndId(response: HttpResponseBase): Observable<ModificationEntryDto[]> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : <ModificationEntryDto[]>JSON.parse(_responseText, this.jsonParseReviver);
+            return _observableOf(result200);
+            }));
+        } else if (status === 404) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("contact not found", status, _responseText, _headers);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<ModificationEntryDto[]>(<any>null);
+    }
 }
 
 @Injectable({
@@ -2679,11 +2746,40 @@ export interface EventCreateDto {
 
 export interface ModificationEntryDto {
     id: number;
+    dataType: DATA_TYPE;
     dataModelType: MODEL_TYPE;
     dataModelId: number;
     modificationType: MODIFICATION;
     userName?: string | undefined;
     dateTime: string;
+    oldValue?: string | undefined;
+    actualValue?: string | undefined;
+    isDeleted: boolean;
+    extensionIndex: number;
+    propertyName?: string | undefined;
+}
+
+export enum DATA_TYPE {
+    MAIL = 0,
+    PHONE = 1,
+    FAX = 2,
+    PHONE_EXTENDED = 3,
+    MAIL_EXTENDED = 4,
+    HISTORY_ELEMENT = 5,
+    NONE = 6,
+    PRENAME = 7,
+    NAME = 8,
+    CITY = 9,
+    COUNTRY = 10,
+    STREET = 11,
+    STREETNUMBER = 12,
+    ZIPCODE = 13,
+    DESCRIPTION = 14,
+    CONTACTS = 15,
+    DURATION = 16,
+    DATE = 17,
+    TIME = 18,
+    PARTICIPATED = 19,
 }
 
 export enum MODEL_TYPE {
@@ -2695,6 +2791,8 @@ export enum MODEL_TYPE {
 export enum MODIFICATION {
     CREATED = 0,
     MODIFIED = 1,
+    DELETED = 2,
+    ADDED = 3,
 }
 
 export interface OrganizationCreateDto {
