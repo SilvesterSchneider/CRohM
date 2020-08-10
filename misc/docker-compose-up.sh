c2f748bf459e29455f201eeaba559939fc9e457b
Pantastisch <45369229+Pantastisch@users.noreply.github.com>
2020-08-10 21:39:58 +0200

    Add latest tag in checkout for master branch

--- misc/docker-compose-up.sh
+++ misc/docker-compose-up.sh
@@ -5,12 +5,17 @@ echo "Starting docker-stack..."
 export CROHM_BRANCH=$(git branch --show-current)
 export CROHM_IMAGE=$(echo "${CROHM_BRANCH}" | tr "/" -)
 
+if [ ${CROHM_BRANCH} = "master" ]; then
+    export CROHM_IMAGE=latest;
+fi
+
 echo
 echo "Current Branch:    ${CROHM_BRANCH}"
 echo "Docker Tag:        ${CROHM_IMAGE}"
 
 echo
 echo "Pull most recent image for branch"
+
 docker pull crohmcrms/crohm_crms:${CROHM_IMAGE}
 
 echo
