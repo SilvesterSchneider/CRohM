c0f266788b020a4c341c6291c4951363b9b2e7da
CHRIST\sischneider <sischneider@christ-es.de>
2020-08-12 15:27:55 +0200

    update

--- backend/ServiceLayer/MailService.cs
+++ backend/ServiceLayer/MailService.cs
@@ -1,4 +1,3 @@
-// Copyright (C) Christ Electronic Systems GmbH
 
 using System;
 using System.Collections.Generic;
@@ -16,6 +15,7 @@ namespace ServiceLayer
     {
         public bool CreateAndSendMail(string address, string subject, string body, byte[] attachment,
             string attachmentType);
+
         public bool PasswordReset(string newPassword, string mailAddress);
 
         public bool Registration(string benutzer, string passwort, string email);
@@ -36,14 +36,14 @@ namespace ServiceLayer
         {
             string body = "<h3> Herzlich Willkommen bei CRohm </h3> " +
                    "<p> Sie wurden als neuer Benutzer im System angelegt.Ihnen wurden folgenden Zugangsdaten zugewiesen:</p> " +
-                   "<p style = \"padding-left: 50px\" > Benutzername: <font size = \"3\" color = \"#0000FF\"><b> " + user + " </b></font></p> " +
-                   "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + password + " </b></font></p> " +
+                   "<p style = \"padding-left: 50px\" > Benutzername: <font size = \"3\" color = \"#0000FF\"><b> " + benutzer + " </b></font></p> " +
+                   "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
                    "<p> Bitte ändern Sie aus Sicherheitsgründen ihr Passwort nach Ihrem ersten Login.</p> " +
                    "<br> " +
                    "<p> Mit freundlichen Grüßen,</p> " +
                    "<p> Ihr CRohm Team.</p>";
 
-            return SendMail("Zugangsdaten", body, email,null, "");
+            return SendMail("Zugangsdaten", body, email, null, "");
         }
 
         public bool SendDataProtectionUpdateMessage(string title, string lastName, string emailAddressRecipient, string data)
@@ -58,7 +58,7 @@ namespace ServiceLayer
                           " </ul> " +
                           " <p> Technische Hochschule Nürnberg</p> ";
 
-            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient);
+            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient, null, "");
         }
 
         public bool SendDataProtectionDeleteMessage(string title, string lastName, string emailAddressRecipient, string data)
@@ -73,14 +73,14 @@ namespace ServiceLayer
                           " </ul> " +
                           " <p> Technische Hochschule Nürnberg</p> ";
 
-            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient);
+            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient, null, "");
         }
 
         public bool PasswordReset(string passwort, string email)
         {
             string body = "<h3> Passwort zurückgesetzt bei CRohm </h3> " +
                           "<p> Ihr Passwort wurde auf Ihre Anfrage zurück gesetzt.Ihr neues Passwort lautet.</p> " +
-                          "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + password + " </b></font></p> " +
+                          "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
                           "<p> Falls sie kein neues Passwort angefragt haben setzten Sie sich bitte mit Ihrem Admin in verbindung.</p> " +
                           "<br> " +
                           "<p> Mit freundlichen Grüßen,</p> " +
@@ -89,7 +89,7 @@ namespace ServiceLayer
             return SendMail("Ihr Passwort wurde zurückgesetzt.", body, email, null, "");
         }
 
-        private bool SendMail(string subject, string body, string mail, Stream attachment, string attachmentType)
+        private bool SendMail(string subject, string body, string emailAddressRecipient, Stream attachment, string attachmentType)
         {
             try
             {
