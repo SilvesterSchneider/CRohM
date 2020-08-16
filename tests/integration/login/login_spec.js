9d33767fa6c675789fb7449c6ca5d4ab059e3223
Michelle Martin <michelle.martin@bearingpoint.com>
2020-08-16 12:33:04 +0200

    Add cypress test framework

---
+++ tests/integration/login/login_spec.js
@@ -0,0 +1,10 @@
+describe('Login', () => {
+    it('successfully loads', () => {
+      cy.visit('/login');
+      cy.get('#mat-input-0').click();
+      cy.get('#mat-input-0').type('admin');
+      cy.get('#mat-input-1').type('@dm1n1stR4tOr');
+      cy.get('.mat-button-wrapper').click();
+    })
+  })
+  
