c16fcbe52e8f4d5044163a126685a844e6a96f28
Michelle Martin <michelle.martin@bearingpoint.com>
2020-08-16 14:25:15 +0200

    Add README and login tests

--- tests/integration/login/login_spec.ts
+++ tests/integration/login/login_spec.ts
@@ -1,9 +1,66 @@
 describe('Login', () => {
-    it('successfully loads', () => {
-      cy.visit('/login');
-      cy.get('#mat-input-0').click();
-      cy.get('#mat-input-0').type('admin');
-      cy.get('#mat-input-1').type('@dm1n1stR4tOr');
-      cy.get('.mat-button-wrapper').click();
-    })
-  })
+  it('does not accept wrong password', () => {
+
+    // Login with credentials admin/wrongpassword
+    doLogin('admin', 'wrongpassword');
+
+    //Wait for cookie storage
+    cy.wait(1000);
+
+    // Validate that no JSON Webtoken has been issued 
+    cy.getCookie('.AspNetCore.Identity.Application').should('not.exist');
+
+    // Validate that url equals baseUrl/login
+    cy.url().should('equal', Cypress.config().baseUrl + '/login');
+
+    // Validate that error message is displayed
+    cy.get('#login-error').should('have.text', 'Login fehlgeschlagen!');
+
+  });
+
+
+  it('logs in successful', () => {
+    // Login with credentials admin/@dm1n1stR4tOr
+    doLogin('admin', '@dm1n1stR4tOr');
+
+    //Wait for cookie storage
+    cy.wait(1000);
+
+    // Validate that JSON Webtoken has been saved
+    cy.getCookie('.AspNetCore.Identity.Application').should('exist');
+
+    // Validate that url equals baseUrl
+    cy.url().should('equal', Cypress.config().baseUrl + '/');
+
+    // Validate that the correct page is displayed by checking the title
+    cy.contains('CRohM - Customer Relationship Management System').should('exist');
+  });
+
+  it('logs out correcly when token is deleted', () => {
+    // Delete all cookies
+    cy.clearCookies();
+    // Reload page
+    cy.reload();
+
+    // Validate that no JSON Webtoken has been issued 
+    cy.getCookie('.AspNetCore.Identity.Application').should('not.exist');
+
+    // Validate that url equals baseUrl/login
+    cy.url().should('equal', Cypress.config().baseUrl + '/login');
+  });
+});
+
+function doLogin(username: string, password: string) {
+  // Navigate to login page (baseUrl from cypress.json is used as base)
+  cy.visit('/login');
+
+  //Click input with id 'login-user'
+  cy.get('#login-user').click();
+  // Type 'admin' into username field
+  cy.get('#login-user').type(username);
+  // Type password into password field
+  cy.get('#login-password').type(password);
+  // Click login button
+  cy.get('#login-button').click();
+
+}
