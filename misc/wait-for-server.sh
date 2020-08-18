e7d08b740143e04e24e686a3b0e1d50995dfcfd4
Pantastisch <45369229+Pantastisch@users.noreply.github.com>
2020-08-18 22:12:43 +0200

    Fix CI bugs

--- misc/wait-for-server.sh
+++ misc/wait-for-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SERVER_TIMEOUT=300
+export SERVER_TIMEOUT=300
 
 while [[ "$(curl --insecure --silent --output /dev/null --write-out ''%{HTTP_CODE}'' https://localhost/health)" != "200" ]]; do
     echo "Waiting ${SERVER_TIMEOUT}s till timeout ..."
