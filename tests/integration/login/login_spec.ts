6b1ec5260a3eb40cd6d6d430fe3c2878cb9c0b85
Pantastisch <45369229+Pantastisch@users.noreply.github.com>
2020-08-19 00:25:22 +0200

    Implement new tests

--- tests/integration/login/login_spec.ts
+++ tests/integration/login/login_spec.ts
@@ -1,10 +1,36 @@
 describe('Login', () => {
+    it('should change password on first login', () => {
+        // Init
+        clearCookies();
+
+        // Login with credentials admin/@dm1n1stR4tOr
+        doLogin('admin', '@dm1n1stR4tOr');
+
+        // Wait for cookie storage
+        cy.wait(1000);
+
+        // Get change password form
+        if (cy.contains('Passwort festlegen'))
+        {
+            // Change password
+            cy.get('#change-password').type('@dm1n1stR4tOr')
+
+            // Accept new password
+            cy.get('#change-button').click();
+
+            // Proof successfull login
+            proofSuccesLogin();
+        }
+    });
+
     it('does not accept wrong password', () => {
+        // Init
+        clearCookies();
 
         // Login with credentials admin/wrongpassword
         doLogin('admin', 'wrongpassword');
 
-        //Wait for cookie storage
+        // Wait for cookie storage
         cy.wait(1000);
 
         // Validate that no JSON Webtoken has been issued
@@ -19,31 +45,19 @@ describe('Login', () => {
     });
 
     it('logs in successful', () => {
+        // Init
+        clearCookies();
+
         // Login with credentials admin/@dm1n1stR4tOr
         doLogin('admin', '@dm1n1stR4tOr');
 
-        //Wait for cookie storage
-        cy.wait(1000);
-
-        // Validate that JSON Webtoken has been saved
-        cy.getCookie('.AspNetCore.Identity.Application').should('exist');
-
-        // Validate that url equals baseUrl
-        cy.url().should('equal', Cypress.config().baseUrl + '/');
-
-        // Validate that the correct page is displayed by checking the title
-        cy.contains('CRohM - Customer Relationship Management System').should('exist');
+        // Proof successfull login
+        proofSuccesLogin();
     });
 
     it('logs out correcly when token is deleted', () => {
-        // Delete all cookies
-        cy.clearCookies();
-
-        // Reload page
-        cy.reload();
-
-        // Validate that no JSON Webtoken has been issued
-        cy.getCookie('.AspNetCore.Identity.Application').should('not.exist');
+        // Init
+        clearCookies();
 
         // Validate that url equals baseUrl/login
         cy.url().should('equal', Cypress.config().baseUrl + '/login');
@@ -54,7 +68,7 @@ function doLogin(username: string, password: string) {
     // Navigate to login page (baseUrl from cypress.json is used as base)
     cy.visit('/login');
 
-    //Click input with id 'login-user'
+    // Click input with id 'login-user'
     cy.get('#login-user').click();
 
     // Type 'admin' into username field
@@ -65,4 +79,29 @@ function doLogin(username: string, password: string) {
 
     // Click login button
     cy.get('#login-button').click();
+}
+
+function clearCookies() {
+        // Delete all cookies
+        cy.clearCookies();
+
+        // Reload page
+        cy.reload();
+
+        // Validate that no JSON Webtoken has been issued
+        cy.getCookie('.AspNetCore.Identity.Application').should('not.exist');
+}
+
+function proofSuccesLogin() {
+        // Wait for cookie storage
+        cy.wait(1000);
+
+        // Validate that JSON Webtoken has been saved
+        cy.getCookie('.AspNetCore.Identity.Application').should('exist');
+
+        // Validate that url equals baseUrl
+        cy.url().should('equal', Cypress.config().baseUrl + '/');
+
+        // Validate that the correct page is displayed by checking the title
+        cy.contains('CRohM - Customer Relationship Management System').should('exist');
 }
