2a8b504fbf2c6861ac27c73e0a009c625985a31c
Silvester <silvester_schneider@gmx.de>
2020-09-02 21:15:44 +0200

    add dialog

---
+++ frontend/src/app/shared/data-protection/dp-disclaimer-dialog/dp-disclaimer-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dp-disclaimer-dialog',
+  templateUrl: './dp-disclaimer-dialog.component.html',
+  styleUrls: ['./dp-disclaimer-dialog.component.scss']
+})
+export class DpDisclaimerDialogComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
