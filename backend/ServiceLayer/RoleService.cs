423beeb236f4314b1835b6794fb60765e3e6d17b
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-09-10 23:17:00 +0200

    fixed the naming of roles if already a existing one is there

--- backend/ServiceLayer/RoleService.cs
+++ backend/ServiceLayer/RoleService.cs
@@ -115,20 +115,18 @@ namespace ServiceLayer
 
         public async Task<IdentityResult> CreateAsync(Role role)
         {
+            if (await CheckIfRoleNameAlreadyExistsAsync(role.Name))
+            {
+                return IdentityResult.Failed(new IdentityError() { Code="401", Description="Role with same name already exists!" });
+            }
             return await manager.CreateAsync(role);
         }
 
         public async Task<IdentityResult> CreateWithClaimsAsync(RoleDto role)
         {
             Role roleToCreate = new Role() { Id=0, Name=role.Name };
-            foreach (Role existingRole in await GetAllRolesAsync())
-            {
-                if (existingRole.Name.Equals(roleToCreate.Name))
-                {
-                    return IdentityResult.Success;
-                }
-            }
-            if (await CreateAsync(roleToCreate) == IdentityResult.Success)
+            IdentityResult result = await CreateAsync(roleToCreate);
+            if (result == IdentityResult.Success)
             {
                 Role createdRole = await FindRoleByNameAsync(roleToCreate.Name);
                 List<Claim> claimsToAdd = new List<Claim>();
@@ -146,12 +144,8 @@ namespace ServiceLayer
                 {
                     await AddClaimAsync(createdRole, newClaim);
                 }
-                return IdentityResult.Success;
             }
-            else
-            {
-                return IdentityResult.Failed(new IdentityError() { Code="301", Description="Error creating a role" });
-            }
+            return result;
         }
 
         public async Task<IdentityResult> DeleteAsync(Role role)
@@ -211,6 +205,16 @@ namespace ServiceLayer
             return await manager.UpdateAsync(role);
         }
 
+        private async Task<bool> CheckIfRoleNameAlreadyExistsAsync(string roleName)
+        {
+            List<Role> allAvailableRoles = await GetAllRolesAsync();
+            if (allAvailableRoles.FirstOrDefault(x => x.Name.Equals(roleName)) == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
         public async Task<IdentityResult> UpdateRoleWithClaimsAsync(RoleDto role)
         {
             Role roleToUpdate = await FindRoleByIdAsync(role.Id);
@@ -220,8 +224,11 @@ namespace ServiceLayer
             }
             if (!roleToUpdate.Name.Equals(role.Name))
             {
-                roleToUpdate.Name = role.Name;
-                await UpdateAsync(roleToUpdate);
+                if (!await CheckIfRoleNameAlreadyExistsAsync(role.Name))
+                {
+                    roleToUpdate.Name = role.Name;
+                    await UpdateAsync(roleToUpdate);
+                }
             }
             
             List<Claim> claimsToDelete = new List<Claim>();
