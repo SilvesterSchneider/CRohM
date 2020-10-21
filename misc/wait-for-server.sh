eb094e75b3995d25f2fdcafc78c50a291edc41a4
Pantastisch <45369229+Pantastisch@users.noreply.github.com>
2020-10-21 19:06:04 +0200

    Add log print before timeout

--- misc/wait-for-server.sh
+++ misc/wait-for-server.sh
@@ -8,6 +8,8 @@ while [[ "$(curl --insecure --silent --output /dev/null --write-out ''%{HTTP_COD
     (( SERVER_TIMEOUT -= 1 ))
 
     if [[ "$SERVER_TIMEOUT" -lt 0 ]]; then
+        # Print logs before exiting with error
+        docker-compose logs crms
         exit 1
     fi
 
