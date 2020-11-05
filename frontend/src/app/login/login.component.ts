c69191d63dffc8cd57e39d379749ed33935ccba5
rama@promik.local <razvan.matis@promik.com>
2020-11-05 16:55:29 +0100

    begin feature

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent {
   password = new FormControl('', Validators.required);
   errorText: string;
   user: UserDto;
+  fieldTextType: boolean = true;
   matDialog: MatDialogRef<ChangePasswordComponent>;
 
   constructor(private jwtService: JwtService, private router: Router, private route: ActivatedRoute, private dialog: MatDialog) { }
@@ -44,5 +45,8 @@ export class LoginComponent {
     if (this.userNameOrEmail.valid && this.password.valid) {
       this.login();
     }
+  }
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
   }
 }
