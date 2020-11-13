62f298abffaa13d7b037c489c5a6832f433084a9
Michelle Martin <michelle.martin@bearingpoint.com>
2020-11-13 18:58:51 +0100

    Add cypress tests

--- tests/integration/03_home/home_spec.ts
+++ tests/integration/03_home/home_spec.ts
@@ -1,23 +1,45 @@
 import { loginAsAdmin } from '../../shared/login';
 
-describe('Home Tests',()=>{
-  
-    it('should appear disclaimer beacause no data protection officer are in system',()=>{
+describe('Home Tests', () => {
+
+    it('should appear disclaimer beacause no data protection officer are in system', () => {
         loginAsAdmin();
-        
+
         cy.url().should('equal', Cypress.config().baseUrl + '/?from=login');
         cy.get('#dataProtectionOfficeDisclaimer').should('have.text', 'Warnmeldung');
         cy.get('#resolveButton').should('exist');
-        cy.get('#ignoreButton').should('exist');        
+        cy.get('#ignoreButton').should('exist');
     });
 
-    it('should not appear disclaimer beacause not from Login',()=>{
+    it('should not appear disclaimer beacause not from Login', () => {
         loginAsAdmin();
-        
-        cy.visit('/')
-        
+
+        cy.visit('/');
+
         cy.url().should('equal', Cypress.config().baseUrl + '/');
         cy.get('#dataProtectionOfficeDisclaimer').should('not.exist');
-      
+
+    });
+
+    it('should switch language to english', () => {
+        loginAsAdmin();
+
+        cy.visit('/');
+        cy.get('.userMenu #language-select .mat-icon').click();
+        cy.get('button').contains('English').click();
+
+        cy.contains('Contacts').should('exist');
+        cy.contains('Organizations').should('exist');
+    });
+
+    it('should switch language to german', () => {
+        loginAsAdmin();
+
+        cy.visit('/');
+        cy.get('.userMenu #language-select .mat-icon').click();
+        cy.get('button').contains('Deutsch').click();
+
+        cy.contains('Kontakte').should('exist');
+        cy.contains('Organisationen').should('exist');
     });
 });
