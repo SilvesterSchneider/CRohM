1513aaae0f3f9f7b9bd7caf1de72aeede6b9e6d0
Michelle Martin <michelle.martin@bearingpoint.com>
2020-11-15 18:41:07 +0100

    Add english mail texts

--- backend/ServiceLayer/ContactService.cs
+++ backend/ServiceLayer/ContactService.cs
@@ -33,20 +33,31 @@ namespace ServiceLayer
         {
             Contact contact = await GetByIdAsync(id);
             string gender = " geehrter Herr ";
+            string genderEn = "Dear Mr ";
             if (contact.Gender == Contact.GenderTypes.FEMALE)
             {
                 gender = " geehrte Frau ";
+                genderEn = "Dear Ms ";
             }
             else if (contact.Gender == Contact.GenderTypes.DIVERS)
             {
                 gender = " geehrt ";
+                genderEn = "Dear Mx ";
             }
 
-            string body = "<h3> Auskunft über gespeicherte Daten </h3> " +
+            string body = "<h5><i> - English version below - </i></h5>" +
+                          "<h3> Auskunft über gespeicherte Daten </h3> " +
                           "<p> Sehr" + gender + contact.Name + ",</p>" +
                           "<p> Sie hatten um eine Auskunft über die zur Ihrer Person in unserem Customer Relationship Management System(CRMS) gespeicherten Daten gebeten. Im angehängten PDF - Dokument erhalten Sie die entsprechende Auskunft gem. Art. 15 EU - DSGVO.</p>" +
                           "<p></p>" +
-                          "<p>Technische Hochschule Nürnberg</p>";
+                          "<p>Technische Hochschule Nürnberg</p>" +
+                          "<h5><i>- English version -</i></h5>" +
+                          "<h3> Information about stored data </h3> " +
+                          "<p>" + genderEn + contact.Name + ",</p>" +
+                          "<p> You asked for information about your personal data stored in our Customer Relationship Management System (CRMS). In the attached PDF - document you will find the corresponding information according to Art. 15 EU - DSGVO.</p>" +
+                          "<p></p>" +
+                          "<p>Nuremberg Institute of Technology</p>";
+
             string pdfBody = await GetContactInformationAsTextAsync(contact.Id);
             pdfBody = pdfBody + "";
             mailProvider.CreateAndSendMail(contact.ContactPossibilities.Mail, "Auskunft über gespeicherte Daten", body,
--- backend/ServiceLayer/MailService.cs
+++ backend/ServiceLayer/MailService.cs
@@ -32,18 +32,32 @@ namespace ServiceLayer
     public class MailService : IMailService
     {
         private static string STARTFIELD = "<Anrede>";
+        private static string STARTFIELDEN = "<title>";
         private static string PRENAMEFIELD = "<Vorname>";
+        private static string PRENAMEFIELDEN = "<firstname>";
         private static string NAMEFIELD = "<Nachname>";
+        private static string NAMEFIELDEN = "lastname>";
         private static string ORGASTART = "Sehr geehrte Damen und Herren des Unternehmens";
+        private static string ORGASTARTEN = "Dear ladies and gentlemen of the company";
         private static string EVENTNAMEFIELD = "<Veranstaltungsname>";
         private static string EVENTDATEFIELD = "<Datum>";
         private static string EVENTTIMEFIELD = "<Uhrzeit>";
-        private static string MAILSETUP = "Sehr geehrter Administrator\r\rDie Einstellungen für den Email-Server wurden erfolgreich " +
-            "übernommen\r\rTechnische Hochschule Nürnberg";
+        private static string MAILSETUP = "<h5><i> - English version below - </i></h5>" +
+            "Sehr geehrter Administrator\r\rDie Einstellungen für den Email-Server wurden erfolgreich " +
+            "übernommen\r\rTechnische Hochschule Nürnberg" +
+            "<h5><i>- English version -</i></h5>" +
+            "Dear Administrator\r\rThe settings for the email server were successfully applied " +
+            "\r\rNuremberg Institute of Technology";
+
         private static string TESTMAIL = "Test-Email";
-        public static string INVITATION_DEF_CONTENT = STARTFIELD + " " + PRENAMEFIELD + " " + NAMEFIELD +
+        public static string INVITATION_DEF_CONTENT = "- English version below -\r" +
+            STARTFIELD + " " + PRENAMEFIELD + " " + NAMEFIELD +
             "\rWir laden Sie herzlich ein zu unserer Veranstaltung \"" + EVENTNAMEFIELD +
-            "\" am " + EVENTDATEFIELD + " um " + EVENTTIMEFIELD + " Uhr.\rWir freuen uns auf Ihr Erscheinen.\rTechnische Hochschule Nürnberg";
+            "\" am " + EVENTDATEFIELD + " um " + EVENTTIMEFIELD + " Uhr.\rWir freuen uns auf Ihr Erscheinen.\rTechnische Hochschule Nürnberg" +
+            "\r\r- English Version -\r" +
+            STARTFIELDEN + " " + PRENAMEFIELDEN + " " + NAMEFIELDEN +
+            "\rWe cordially invite you to our event \"" + EVENTNAMEFIELD +
+            "\" on " + EVENTDATEFIELD + " at " + EVENTTIMEFIELD + ".\rWe look forward to your appearance.\rNuremberg Institute of Technology";
 
         public bool CreateAndSendMail(string address, string subject, string body, byte[] attachment, string attachmentType)
         {
@@ -57,21 +71,34 @@ namespace ServiceLayer
 
         public bool Registration(string benutzer, string passwort, string email)
         {
-            string body = "<h3> Herzlich Willkommen bei CRohm </h3> " +
+            string body = "<h5><i> - English version below - </i></h5>" +
+                   "<h3> Herzlich Willkommen bei CRohm </h3> " +
                    "<p> Sie wurden als neuer Benutzer im System angelegt.Ihnen wurden folgenden Zugangsdaten zugewiesen:</p> " +
                    "<p style = \"padding-left: 50px\" > Benutzername: <font size = \"3\" color = \"#0000FF\"><b> " + benutzer + " </b></font></p> " +
                    "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
                    "<p> Bitte ändern Sie aus Sicherheitsgründen ihr Passwort nach Ihrem ersten Login.</p> " +
                    "<br> " +
                    "<p> Mit freundlichen Grüßen,</p> " +
-                   "<p> Ihr CRohm Team.</p>";
+                   "<p> Ihr CRohm Team.</p>" +
+                   "<br" +
+                   "<h5><i>- English version -</i></h5>" +
+                   "<h3> Welcome to CRohm </h3> " +
+                   "<p> You were created as a new user in the system and assigned the following access data:</p> " +
+                   "<p style = \"padding-left: 50px\" > Username: <font size = \"3\" color = \"#0000FF\"><b> " + benutzer + " </b></font></p> " +
+                   "<p style = \"padding-left: 50px\" > Password: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
+                   "<p> For security reasons please change your password after your first login.</p> " +
+                   "<br> " +
+                   "<p> With kind regards,</p> " +
+                   "<p> Your CRohm Team.</p>";
 
-            return SendMail("Zugangsdaten", body, email, null, "");
+
+            return SendMail("Zugangsdaten / Access Data", body, email, null, "");
         }
 
         public bool SendDataProtectionUpdateMessage(string title, string lastName, string emailAddressRecipient, string data)
         {
-            string body = $"<p>Sehr geehrte/r {title} {lastName}</p> " +
+            string body = $"<h5><i> - English version below - </i></h5>" +
+                          "<p>Sehr geehrte/r {title} {lastName}</p> " +
                           " <p> Sie hatten um Änderung bzw. Löschung von zur Ihrer Person in " +
                           "       unserem Customer Relationship Management System(CRMS) " +
                           "       gespeicherten Daten gebeten.</p> " +
@@ -79,14 +106,27 @@ namespace ServiceLayer
                           " <ul> " +
                           data +
                           " </ul> " +
-                          " <p> Technische Hochschule Nürnberg</p> ";
+                          " <p> Technische Hochschule Nürnberg</p> " +
+                          "<br>" +
+                          "<h5><i>- English version -</i></h5>" +
+                          "<p>Dear Sir or Madam {title} {lastName}</p> " +
+                          " <p> You have requested that your personal data stored " +
+                          "       in our Customer Relationship Management System (CRMS) " +
+                          "       be changed or deleted.</p> " +
+                          " <p> The following dates were changed:</p> " +
+                          " <ul> " +
+                          data +
+                          " </ul> " +
+                          " <p> Nuremberg Institute of Technology</p> ";
 
-            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient, null, "");
+
+            return SendMail("Mitteilung über Änderung oder Löschung von Daten / Notification of change or deletion of data", body, emailAddressRecipient, null, "");
         }
 
         public bool SendDataProtectionDeleteMessage(string title, string lastName, string emailAddressRecipient, string data)
         {
-            string body = $"<p>Sehr geehrte/r {title} {lastName}</p> " +
+            string body = $"<h5><i> - English version below - </i></h5>" +
+                          "<p>Sehr geehrte/r {title} {lastName}</p> " +
                           " <p> Sie hatten um Änderung bzw. Löschung von zur Ihrer Person in " +
                           "       unserem Customer Relationship Management System(CRMS) " +
                           "       gespeicherten Daten gebeten.</p> " +
@@ -94,22 +134,43 @@ namespace ServiceLayer
                           " <ul> " +
                           data +
                           " </ul> " +
-                          " <p> Technische Hochschule Nürnberg</p> ";
+                          " <p> Technische Hochschule Nürnberg</p> " +
+                          "<br>" +
+                          "<h5><i>- English version -</i></h5>" +
+                          "<p>Dear Sir or Madam {title} {lastName}</p> " +
+                          " <p> You have requested that your personal data stored " +
+                          "       in our Customer Relationship Management System (CRMS) " +
+                          "       be changed or deleted.</p> " +
+                          " <p> The following dates were deleted:</p> " +
+                          " <ul> " +
+                          data +
+                          " </ul> " +
+                          " <p> Nuremberg Institute of Technology</p> ";
 
-            return SendMail("Mitteilung über Änderung oder Löschung von Daten", body, emailAddressRecipient, null, "");
+            return SendMail("Mitteilung über Änderung oder Löschung von Daten / Notification of change or deletion of data", body, emailAddressRecipient, null, "");
         }
 
         public bool PasswordReset(string passwort, string email)
         {
-            string body = "<h3> Passwort zurückgesetzt bei CRohm </h3> " +
+            string body = "<h5><i> - English version below - </i></h5>" +
+                          "<h3> Passwort zurückgesetzt bei CRohm </h3> " +
                           "<p> Ihr Passwort wurde auf Ihre Anfrage zurück gesetzt.Ihr neues Passwort lautet.</p> " +
                           "<p style = \"padding-left: 50px\" > Passwort: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
                           "<p> Falls sie kein neues Passwort angefragt haben setzten Sie sich bitte mit Ihrem Admin in verbindung.</p> " +
                           "<br> " +
                           "<p> Mit freundlichen Grüßen,</p> " +
-                          "<p> Ihr CRohm Team.</p> ";
+                          "<p> Ihr CRohm Team.</p> " +
+                          "<br>" +
+                          "<h5><i>- English version -</i></h5>" +
+                          "<h3> Password reset for CRohm </h3> " +
+                          "<p> Your password was reset to your request. Your new password is.</p> " +
+                          "<p style = \"padding-left: 50px\" > Password: <font size = \"3\" color = \"#0000FF\" ><b> " + passwort + " </b></font></p> " +
+                          "<p> If you have not requested a new password, please contact your admin.</p> " +
+                          "<br> " +
+                          "<p> With kind regards,</p> " +
+                          "<p> Your CRohm Team.</p> ";
 
-            return SendMail("Ihr Passwort wurde zurückgesetzt.", body, email, null, "");
+            return SendMail("Ihr Passwort wurde zurückgesetzt / Your password has been reset", body, email, null, "");
         }
 
         private bool SendMail(string subject, string body, string emailAddressRecipient, Stream attachment, string attachmentType)
@@ -155,16 +216,20 @@ namespace ServiceLayer
         public bool CreateAndSendInvitationMail(string address, string preName, string name, string mailContent, GenderTypes gender)
         {
             string start = "Sehr geehrter Herr";
+            string genderEn = "Dear Mr ";
             if (gender == GenderTypes.FEMALE)
             {
                 start = "Sehr geehrte Frau";
+                genderEn = "Dear Ms ";
             }
             else if (gender == GenderTypes.DIVERS)
             {
                 start = "Sehr geehrt";
+                genderEn = "Dear Mx ";
             }
             string finishedcontent = mailContent.Replace(NAMEFIELD, name).Replace(STARTFIELD, start).Replace(PRENAMEFIELD, preName);
-            return SendFormattedMail("Einladung zur Veranstaltung", finishedcontent, address, null, null);
+            finishedcontent = finishedcontent.Replace(NAMEFIELDEN, name).Replace(STARTFIELDEN, genderEn).Replace(PRENAMEFIELDEN, preName);
+            return SendFormattedMail("Einladung zur Veranstaltung / Invitation to the event", finishedcontent, address, null, null);
         }
 
         public async Task<bool> SendMailToAddress(string subject, string address, string mailContent)
@@ -192,15 +257,18 @@ namespace ServiceLayer
         public bool CreateAndSendInvitationMail(string mail, string name, string mailContent)
         {
             string finishedcontent = mailContent.Replace(STARTFIELD, ORGASTART);
+            finishedcontent = finishedcontent.Replace(STARTFIELDEN, ORGASTARTEN);
             if (mailContent.IndexOf(NAMEFIELD) > 0)
             {
                 finishedcontent = finishedcontent.Replace(NAMEFIELD, name).Replace(PRENAMEFIELD, string.Empty);
+                finishedcontent = finishedcontent.Replace(NAMEFIELDEN, name).Replace(PRENAMEFIELDEN, string.Empty);
             }
             else
             {
                 finishedcontent = finishedcontent.Replace(NAMEFIELD, string.Empty).Replace(PRENAMEFIELD, name);
+                finishedcontent = finishedcontent.Replace(NAMEFIELDEN, string.Empty).Replace(PRENAMEFIELDEN, name);
             }
-            return SendFormattedMail("Einladung zur Veranstaltung", finishedcontent, mail, null, null);
+            return SendFormattedMail("Einladung zur Veranstaltung / Invitation to the event", finishedcontent, mail, null, null);
         }
     }
 }
